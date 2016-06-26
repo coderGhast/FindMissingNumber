9e56300a62feb66f27fcadb8bf847c862ac6af41
James <euesdenje@gmail.com>
2016-06-25 18:14:06 -0600

    Added tests and code for sorted and summed
    The two other methods aside from looping are to sum the list and take
    away the result from the sum of the natural list, and to sort the list
    and find the number missing.
    Summing the list is the best and most efficient way.
    Insertion sort code isn't mine. I studied it, I implemented it once or
    twice, but for ease I just took it from elsewhere as it wasn't the focus
    of this playing around.

--- js/missing-number-finder.js
+++ js/missing-number-finder.js
@@ -19,3 +19,43 @@ function find_number_loop(a){
         return missing_number;
     }
 }
+
+function find_number_sum(a){
+    if(a === undefined || a === null){
+        return 0;
+    } else {
+        var total = 0;
+        for(var i=0; i < a.length; i++){
+            total += a[i];
+        }
+
+        var missing_number = ((a.length +1) * (a.length + 2)/ 2) -  total;
+        return missing_number;
+    }
+}
+
+function find_number_sorted(a){
+    if(a === undefined || a === null){
+        return 0;
+    } else {
+        insertion_sort(a);
+        for(var i=0; i<a.length; i++){
+            if(a[i] !== i + 1){
+                return i + 1;
+            }
+        }
+    }
+}
+
+// Just keep it simple with Insertion sort. Not the best or most efficient but there we go.
+// http://codingmiles.com/sorting-algorithms-insertion-sort-using-javascript/ for insertion sort
+function insertion_sort(unsorted_list) {  
+  for(var i = 0; i < unsorted_list.length; i++) {
+    var temp = unsorted_list[i];
+    for(var j = i - 1; j >= 0 && unsorted_list[j] > temp; j--) {
+      unsorted_list[j +1 ] = unsorted_list[j];
+    }
+    unsorted_list[j+1] = temp;
+  }
+  return unsorted_list;
+}
--- js/tests.js
+++ js/tests.js
@@ -1,15 +1,47 @@
-QUnit.test("should return zero when array is undefined", function( assert ) {
+QUnit.test("sum should return zero when array is undefined", function( assert ) {
+    var test_array;
+    assert.equal(find_number_sum(test_array), 0, "Returned zero when array was undefined; equal succeeds");
+});
+
+QUnit.test("sum should return zero when array is null", function( assert ) {
+    var test_array = null;
+    assert.equal(find_number_sum(test_array), 0, "Returned zero when array was null; equal succeeds");
+});
+
+QUnit.test("sum should find the missing number", function( assert ) {
+    var test_array = [2, 9, 1, 7, 5, 3, 4, 6, 10];
+    var missing_number_result = 8;
+    assert.equal(find_number_sum(test_array), missing_number_result, "Returned the missing number which was 4: " + missing_number_result);
+});
+
+QUnit.test("looper should return zero when array is undefined", function( assert ) {
     var test_array;
     assert.equal(find_number_loop(test_array), 0, "Returned zero when array was undefined; equal succeeds");
 });
 
-QUnit.test("should return zero when array is null", function( assert ) {
+QUnit.test("looper should return zero when array is null", function( assert ) {
     var test_array = null;
     assert.equal(find_number_loop(test_array), 0, "Returned zero when array was null; equal succeeds");
 });
 
-QUnit.test("should find the missing number", function( assert ) {
-    var test_array = [2, 1, 5, 3];
-    var missing_number_result = 4;
+QUnit.test("looper should find the missing number", function( assert ) {
+    var test_array = [2, 9, 1, 7, 5, 3, 4, 6, 10];
+    var missing_number_result = 8;
     assert.equal(find_number_loop(test_array), missing_number_result, "Returned the missing number which was 4: " + missing_number_result);
+});
+
+QUnit.test("sorter should return zero when array is undefined", function( assert ) {
+    var test_array;
+    assert.equal(find_number_sorted(test_array), 0, "Returned zero when array was undefined; equal succeeds");
+});
+
+QUnit.test("sorter should return zero when array is null", function( assert ) {
+    var test_array = null;
+    assert.equal(find_number_sorted(test_array), 0, "Returned zero when array was null; equal succeeds");
+});
+
+QUnit.test("sorter should find the missing number", function( assert ) {
+    var test_array = [2, 9, 1, 7, 5, 3, 4, 6, 10];
+    var missing_number_result = 8;
+    assert.equal(find_number_sorted(test_array), missing_number_result, "Returned the missing number which was 4: " + missing_number_result);
 });
